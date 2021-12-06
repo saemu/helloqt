6367661e2f4903227506c46f21e8299c6144ccab
Samuel Brand <saemu@users.noreply.github.com>
2021-12-06 14:51:54 +0100

    use Qt6 package provided by conan.io

--- cmake/compiler_settings.cmake
+++ cmake/compiler_settings.cmake
@@ -1,7 +1,7 @@
 
 add_library( compiler_settings INTERFACE )
 
-target_compile_features( compiler_settings INTERFACE cxx_std_14 )
+target_compile_features( compiler_settings INTERFACE cxx_std_17 )
 
 target_compile_options( compiler_settings INTERFACE
                         $<$<CXX_COMPILER_ID:MSVC>:/W4;/WX>
